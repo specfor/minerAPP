8e5fb5e1a93b88fbf98b1c96508e25afa32164be
specfor <vihanga2003nimsara@gmail.com>
2022-01-17 00:50:20 +0530

    exit disapper fix few other fixes

--- main.js
+++ main.js
@@ -18,9 +18,8 @@ function createWindow () {
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
   mainWindow.setMenu(null)
-
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
