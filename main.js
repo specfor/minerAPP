ad74ebd76e1f38a5ffc716537c450534b9977e52
specfor <vihanga2003nimsara@gmail.com>
2022-02-22 13:56:27 +0530

    merge

--- main.js
+++ main.js
@@ -209,12 +209,7 @@ async function runEngine(engine_name, coin_name){
 
   let engine = child.spawn(executable_path, {detached: true, stdio: 'ignore', cwd: engine_details['path']});
   engine_pid = engine.pid;
-<<<<<<< HEAD
   console.log(engine_pid)
-=======
-  // console.log(engine_pid)
-  
->>>>>>> 888930cd1021881e276c24627bbb6aec857fc9ca
   engine.on('exit', (code) => {
     console.log(`Miner program exited with code ${code}`);
   });
