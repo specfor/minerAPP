ad733e5f7ef8ac4f629daf9ceb8cace32c211d5c
scriptbreaker <vihanga2003nimsara>
2023-02-25 12:16:33 +0530

    tab selection done

--- assets/js/index_embed.js
+++ assets/js/index_embed.js
@@ -1,14 +1,19 @@
 // Handling tabs
 
-const tabs = document.getElementsByClassName("tabs");
+// set home tab to show
+document.getElementById('home').style.display = "block";
+
+const tabs = document.getElementsByClassName("tab");
+const tabNavBtns = document.getElementsByClassName("nav-btn");
+console.log(tabs.length);
 
 // Default tab is home
-var lastTabId = "home"
+var lastTabId = "home";
 
-tabs.forEach(tab => {
-    tab.addEventListener("click", () => {
-        tab.style.display = "block";
+for (let index = 0; index < tabNavBtns.length; index++) {
+    tabNavBtns.item(index).addEventListener("click", ()=>{
+        document.getElementById(tabNavBtns.item(index).id.substr(4)).style.display = "block";
         document.getElementById(lastTabId).style.display = "none";
-        lastTabId = tab.id;
-    })
-});
+        lastTabId = tabNavBtns.item(index).id.substr(4);
+    })    
+}
