0b6255e747134d4d61d1b94cca3307a7ad704904
scriptbreaker <vihanga2003nimsara>
2023-02-25 14:12:01 +0530

    made non resizable

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 500,
+    resizable: false,
     // webPreferences: {
     //   preload: path.join(__dirname, 'preload.js')
     // }
