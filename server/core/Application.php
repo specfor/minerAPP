47b6a126b4f66eed157e12160282af263cb61b02
scriptbreaker <vihanga2003nimsara>
2023-02-28 10:48:43 +0530

    basic request and response handling classes added

--- server/core/Application.php
+++ server/core/Application.php
@@ -4,19 +4,40 @@ require_once 'Database.php';
 require_once 'Router.php';
 require_once 'User.php';
 require_once 'Session.php';
+require_once 'Request.php';
+require_once 'Response.php';
 
 class Application
 {
     public static Application $app;
     public Database $db;
     public Router $router;
+    public User $user;
+    public Request $request;
+    public Response $response;
+
 
     public function __construct($config)
     {
         self::$app = $this;
 
-        $this->router = new Router();
+        $this->response = new Response();
+        $this->request = new Request();
+        $this->router = new Router($this->request, $this->response);
         $this->db = new Database($config['db']['servername'], $config['db']['dbname'],
             $config['db']['username'], $config['db']['password']);
+
+    }
+
+    public function run()
+    {
+//        try {
+//            echo $this->router->resolveRoute();
+//        } catch (Exception $e) {
+//            $this->response->setStatusCode($e->getCode());
+//            echo $this->view->renderView('_error', [
+//                'exception' => $e
+//            ]);
+//        }
     }
 }
--- server/core/Router.php
+++ server/core/Router.php
@@ -3,5 +3,28 @@
 
 class Router
 {
+    public Request $request;
+    public Response $response;
+    protected array $routes = [];
+
+    public function __construct(Request $request, Response $response)
+    {
+        $this->request = $request;
+        $this->response = $response;
+    }
+
+    public function addGetRoute($path, $callback)
+    {
+        $this->routes['get'][$path] = $callback;
+    }
+
+    public function addPostRoute($path, $callback)
+    {
+        $this->routes['post'][$path] = $callback;
+    }
+
+    public function resolveRoute(){
+//        todo: do the relevant things according to the url
+    }
 
 }
