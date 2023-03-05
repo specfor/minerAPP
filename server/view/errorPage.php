873b6526a161131a986953c1a369251fd8bc329d
Chanuka Nuhan <chanukanuhan@gmail.com>
2023-03-05 19:13:11 +0530

    error page

--- server/view/errorPage.php
+++ server/view/errorPage.php
@@ -1,3 +1,11 @@
 <div class="conntainer pt-5">
-    <h1>{{errorPage:err-message}}</h1>
+<h1>Page Not Found:</h1>
+    <div class="container pt-4">
+      <span>
+        <img src="images/error head image.jpg" alt="" style="height:400px;width: 500px;">
+      </span>
+        <span>
+        <img src="images/error image 404.png" alt="error:404" style="height:300px;width: 500px;">
+      </span>
+    </div>
 </div>
